ee936a859da049a6f8a5eaa300bd597c9379e94f
Shirley Urban <shirleyurban@outlook.de>
2021-05-01 16:22:52 +0200

    jest tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,33 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const service = require('./service')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/add', (req, res) => {
     const { num1, num2 } = req.body
-    let result = num1 + num2
+    const result = service.add(num1, num2)
     res.json({ result })
 })
 
 app.post('/subtract', (req, res) => {
     const { num1, num2 } = req.body
-    let result = num1 - num2
+    const result = service.subtract(num1, num2)
     res.json({ result })
 })
 
 app.post('/multiply', (req, res) => {
     const { num1, num2 } = req.body
-    let result = num1 * num2
+    const result = service.multiply(num1, num2)
     res.json({ result })
 })
 
 app.post('/divide', (req, res) => {
     const { num1, num2 } = req.body
-    let result = num1 / num2
+    const result = service.divide(num1, num2)
     res.json({ result })
 })
 
---
+++ backend/src/service.test.js
@@ -0,0 +1,17 @@
+const service = require('./service')
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(service.add(1, 2)).toBe(3);
+});
+
+test('substracts 3 - 2 to equal 1', () => {
+    expect(service.subtract(3, 2)).toBe(1);
+});
+
+test('multiplies 2 * 2 to equal 4', () => {
+    expect(service.multiply(2, 2)).toBe(4);
+});
+
+test('divides 6 / 3 to equal 2', () => {
+    expect(service.divide(6, 3)).toBe(2);
+});
